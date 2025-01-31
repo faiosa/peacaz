8fb3b977cba33925905114db78a46da11fd2a11b
Oleh Myhal <odmyhal@gmail.com>
2025-01-31 11:04:10 +0200

    tweak update

--- config/tweak.py
+++ config/tweak.py
@@ -10,14 +10,14 @@ class Tweak():
         self.name = "tweak"
         self.paths = [
             os.path.join(os.path.dirname(str(QStandardPaths.writableLocation(QStandardPaths.AppConfigLocation))), self.domain, self.name + ".json"),
-            os.path.join("_internal", "config", self.name + ".json"),
-            os.path.join("config", self.name + ".json")
+            os.path.join(os.path.dirname(__file__), self.name + ".json")
         ]
 
 
 
     def get_settings(self):
         for p in self.paths:
+            print(f"CHECKING path = {os.path.abspath(p)}")
             if os.path.isfile(p):
                 try:
                     with open(os.path.abspath(p), "r", encoding="utf-8") as f:
@@ -29,6 +29,7 @@ class Tweak():
     def write_settings(self, settings):
         path = self.paths[0]
         os.makedirs(os.path.dirname(path), exist_ok=True)
+        print(f"WRITING to path = {path}")
         with open(self.paths[0], "w", encoding="utf-8") as file:
             json.dump(settings, file, indent=4, ensure_ascii=False)
             file.close()
